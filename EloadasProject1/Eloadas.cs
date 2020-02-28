3459d650a1516268b970bd2efea2b945d2d3a277
Tordon <Tordon@DESKTOP-G4ANKN4>
2020-02-28 17:03:10 +0100

    Add project files.

---
+++ EloadasProject1/Eloadas.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace EloadasProject
+{
+
+    class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        int foglalas = 0;
+        int sorokSzama;
+        int helyekSzama;
+
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (sorokSzama > 0 && helyekSzama > 0)
+            {
+                this.sorokSzama = sorokSzama;
+                this.helyekSzama = helyekSzama;
+                foglalasok = new bool[sorokSzama, helyekSzama];
+            }
+            else
+            {
+                ArgumentException e;
+            }
+        }
+        public bool Lefoglal()
+        {
+            if (SzabadHelyek > 0)
+            {
+                foglalas++;
+                return true;
+            }
+            return false;
+        }
+        public int SzabadHelyek
+        {
+            get
+            {
+                return (helyekSzama * sorokSzama) - foglalas;
+            }
+        }
+        public bool Teli
+        {
+            get
+            {
+                return (helyekSzama * sorokSzama) == foglalas;
+            }
+        }
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam > 0 && helySzam > 0)
+            {
+
+            }
+            else
+            {
+                ArgumentException e;
+            }
+            return false;
+        }
+    }
+
+
+}
---
+++ EloadasProject1/Teszt/EloadasTeszt.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EloadasProject
+{
+    [TestFixture]
+    class EloadasTest
+
+    {
+        Eloadas e;
+        [SetUp]
+        public void SetUp()
+        {
+            e = new Eloadas(2, 2);
+        }
+        [TestCase]
+        public void UjEloadasMindenHelyUres()
+        {
+            Assert.AreEqual(4, e.SzabadHelyek, "Üres előadas nem üres minden hely");
+            Eloadas r2 = new Eloadas(4, 4);
+            Assert.AreEqual(16, r2.SzabadHelyek, "Üres előadasd me, üres minden hely");
+        }
+        [TestCase]
+        public void EloadasSzabadhelyekSzamaFoglal()
+        {
+            e.Lefoglal();
+            e.Lefoglal();
+            Assert.AreEqual(2, e.SzabadHelyek, "Üres helyre, üres minden hely");
+        }
+        [TestCase]
+        public void UjEloadasNincsTeli()
+        {
+            Assert.IsFalse(e.Teli, "Új előadas teli van");
+        }
+        [TestCase]
+        public void UjEloadasTeleVan()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                e.Lefoglal();
+            }
+            Assert.IsTrue(e.Teli, "Új előadas teli van");
+        }
+        [TestCase]
+        public void TeliEloadasHelyFoglalas()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                e.Lefoglal();
+            }
+            e.Lefoglal();
+            Assert.IsTrue(e.Teli, "Új repülő teli van");
+        }
+        
+    }
+}
